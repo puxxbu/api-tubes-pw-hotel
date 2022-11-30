ee5d797d171afea4b04118e1163d56f3c8e54687
puxxbu <puxxbu@users.noreply.github.com>
2022-11-30 12:09:26 +0700

    first commit

---
+++ app/Http/Controllers/api/AuthController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Validator;
+
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $request -> validate([
+            'name' => 'required',
+            'email' => 'required|unique:users',
+            'password' => 'required'
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => $request->password,
+            
+        ]);
+
+        $token = $user->createToken('auth-sanctum')->plainTextToken;
+        
+        return response()->json([
+            'data'=> $user,
+            'access_token'=> $token,
+            'token_type' => 'Bearer'
+        ]);
+    }
+}
