34d9bae50f60982440754965934fd8b7aab89190
puxxbu <puxxbu@users.noreply.github.com>
2022-12-01 16:05:48 +0700

    add progress

--- app/Http/Controllers/api/AuthController.php
+++ app/Http/Controllers/api/AuthController.php
@@ -6,13 +6,15 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Facades\Validator;
+use App\Http\Requests\LoginUserRequest;
+use Illuminate\Support\Facades\Auth;
 
 
 class AuthController extends Controller
 {
     public function register(Request $request)
     {
-        $request -> validate([
+        $request->validate([
             'name' => 'required',
             'email' => 'required|unique:users',
             'password' => 'required'
@@ -22,14 +24,31 @@ class AuthController extends Controller
             'name' => $request->name,
             'email' => $request->email,
             'password' => $request->password,
-            
+
         ]);
 
         $token = $user->createToken('auth-sanctum')->plainTextToken;
-        
+
         return response()->json([
-            'data'=> $user,
-            'access_token'=> $token,
+            'data' => $user,
+            'access_token' => $token,
+            'token_type' => 'Bearer'
+        ]);
+    }
+
+    public function login(LoginUserRequest $request)
+    {
+        $request->validate($request->all());
+
+        if (!Auth::attempt($request->only(['email', 'password']))) {
+            return $this->error('', 'Credentials dont match', 401);
+        }
+
+        $user = User::where('email', $request->email)->first();
+
+        return response()->json([
+            'data' => $user,
+            'access_token' => $user->createToken('Api Token of' . $user->name)->plainTextToken,
             'token_type' => 'Bearer'
         ]);
     }
