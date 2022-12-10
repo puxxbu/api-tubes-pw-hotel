e2e5b6a7505aa7f4a8953b065a1aeb1288238cef
puxxbu <puxxbu@users.noreply.github.com>
2022-12-10 22:48:25 +0700

    fix login verified

--- routes/web.php
+++ routes/web.php
@@ -19,9 +19,9 @@ Route::get('/', function () {
 });
 
 
-Route::get('/login', function () {
-    return view('auth.login');
-});
+// Route::get('/login', function () {
+//     return view('auth.login');
+// });
 
 // Route::get('/login', function () {
 //     return view('welcome');
